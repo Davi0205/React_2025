7d960a1807c61d216fc0431d2bc12c95fb32217d
Davi0205 <macedorodriguez.davi@gmail.com>
2025-05-08 22:58:56 -0500

    Cambios en los botones

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react';
 
-const root = createRoot(document.getElementById('root'))
-root.render("Hola muindo nuevo :)")
+// import { createRoot } from 'react-dom/client'
+
+// const root = createRoot(document.getElementById('root'))
+// root.render( 
+//   <StrictMode>
+//     <button>Hola mundonp</button>
+//   </StrictMode>
+// )
+
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <React.Fragment>
+      <button>Hola Mundo1</button>
+      <button>Hola Mundo3</button>
+      <button>Hola Mundo2</button>
+    </React.Fragment>
+);
