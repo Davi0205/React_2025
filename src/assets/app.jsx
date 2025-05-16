d449bca8ddc6600a8e9dc2e48764274f38c5d104
Davi0205 <macedorodriguez.davi@gmail.com>
2025-05-15 19:30:29 -0500

    cambiosuwu

---
+++ src/assets/app.jsx
@@ -0,0 +1,19 @@
+export function App() {
+  return (
+    <article>
+        <header>
+            <img src="/imagenes/Navia_Avatar.jpg" alt="" />
+            <div>
+                <strong>Navia</strong>
+                <span>@Navia</span>
+            </div>
+        </header>
+
+        <aside>
+            <button>
+                Seguir
+            </button>
+        </aside>
+    </article>
+  );
+}
